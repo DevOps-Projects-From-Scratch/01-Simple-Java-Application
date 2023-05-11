8af69b1e263259a1cc1cf3c8799bb1666e07c783
sampath <sampath853@gmail.com>
2023-05-11 10:31:45 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Congratulations You Have Successfully Deployed Java-Applications " );
+    }
+}
